531759ca754f2c701fbbf4bc807bf916c7141cf9
MarwanAhmed154A <marwanahmed154a@gmail.com>
2025-05-26 01:30:05 +0300

    Update ReflectionManager.cpp
    fixed comment typo

--- src/Header Tool/ReflectionManager.cpp
+++ src/Header Tool/ReflectionManager.cpp
@@ -26,7 +26,7 @@ namespace RG
 
 	int ReflectionManager::Add(ReflectedTypeData* a)
 	{
-		//initialize lists as int, safety from unkown static-init order and UB, on static defintion lines to avoid multiple assignments of value
+		//initialize lists as vector*, for safety from unkown static-init order and UB, on static defintion lines to avoid multiple assignments of value
 		static std::vector<BaseSceneObject*>*   types              = s_types              = new std::vector<BaseSceneObject*>();
 		static std::vector<ReflectedTypeData*>* reflectionDataList = s_reflectionDataList = new std::vector<ReflectedTypeData*>();
 
